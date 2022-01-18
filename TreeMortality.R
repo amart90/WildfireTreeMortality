7e2a386ef2cf98ee99c706b073be606bb07105af
amart90 <anthonymartinez90@gmail.com>
2022-01-18 08:47:19 -0800

    Clean up code
    Clean up code to make more readable and consistent.

--- TreeMortality.R
+++ TreeMortality.R
@@ -1,16 +1,18 @@
-##########  Wildfire tree mortality detection and  ##########
-##########   prediction using LiDAR and Landsat    ##########
-##########         Senior capstone project         ##########
-##########             Anthony Martinez            ##########
-##########                June 2017                ##########
+##############################################################
+#   Wildfire tree mortality detection and prediction using   #
+#                     LiDAR and Landsat                      #
+#                  Senior capstone project                   #
+#                      Anthony Martinez                      #
+#                         June 2017                          #
+##############################################################
 
-
+###   Initial Setup   ###
 setwd("/media/antho/HDD/KingFire/CapstoneU")
-#set up to run LiDAR data using parallel processing. Must be using Linux system.
+# set up to run LiDAR data using parallel processing. Must be using Linux system.
 
 ######  Post-fire burn mortality classification  ######
 
-#load packages
+# Load packages
 library(sp)
 library(raster)
 library(rgdal)
@@ -21,14 +23,12 @@ library(doMC)
 library(randomForest)
 library(rgeos)
 
-#identify number of cores for parallel processing
+# Identify number of cores for parallel processing
 registerDoMC(cores = 6)
 
-#load Landsat rasters
-
+# Load Landsat rasters
 preB5i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332013291LGN00-PRE/LC80430332013291LGN00_B5.TIF')
 preB7i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332013291LGN00-PRE/LC80430332013291LGN00_B7.TIF')
-
 postB1i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332014294LGN00-POST/LC80430332014294LGN00_B1.TIF')
 postB2i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332014294LGN00-POST/LC80430332014294LGN00_B2.TIF')
 postB3i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332014294LGN00-POST/LC80430332014294LGN00_B3.TIF')
@@ -36,10 +36,8 @@ postB4i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332
 postB5i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332014294LGN00-POST/LC80430332014294LGN00_B5.TIF')
 postB6i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332014294LGN00-POST/LC80430332014294LGN00_B6.TIF')
 postB7i = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/Initial/LC80430332014294LGN00-POST/LC80430332014294LGN00_B7.TIF')
-
 preB5y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332014246LGN00-PRE/LC80430332014246LGN00_B5.TIF')
 preB7y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332014246LGN00-PRE/LC80430332014246LGN00_B7.TIF')
-
 postB1y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332015249LGN00-POST/LC80430332015249LGN00_B1.TIF')
 postB2y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332015249LGN00-POST/LC80430332015249LGN00_B2.TIF')
 postB3y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332015249LGN00-POST/LC80430332015249LGN00_B3.TIF')
@@ -49,81 +47,84 @@ postB6y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332
 postB7y = raster('/media/antho/HDD/KingFire/Capstone/Landsat8/1YrPost/LC80430332015249LGN00-POST/LC80430332015249LGN00_B7.TIF')
 files <- ls()
 
-#clip rasters
+# Clip rasters
 KingBuffer <- readOGR('/media/antho/HDD/KingFire/Capstone/R/Capstone/KingBuffer.shp')
 x <- foreach(i=1:length(files), .export = files) %dopar% {
   assign(files[[i]], crop(get(files[[i]]), extent(KingBuffer)))
   assign(files[[i]], mask(get(files[[i]]), KingBuffer))}
 for(i in 1:length(files)) {assign(files[[i]], x[[i]])}
 
-#NBR, initial
+## Calculate vegetation indices
+# NBR, initial
 preNBRi <- (1000 * ((preB5i - preB7i) / (preB5i + preB7i)))
 postNBRi <- (1000 * ((postB5i - postB7i) / (postB5i + postB7i)))
 
-#NBR, 1 year later
+# NBR, 1 year later
 preNBRy <- (1000 * ((preB5y - preB7y) / (preB5y + preB7y)))
 postNBRy <- (1000 * ((postB5y - postB7y) / (postB5y + postB7y)))
 
-##dNBR (corr)
-##mode calculation i
-#modei1 <- (preNBRi - postNBRi)
-#ddi <- density(modei1)
-#which.max(ddi$y)
-#modei <- ddi$x[which.max(ddi$y)]
-##mode calculation y
-#modey1 <- (preNBRy - postNBRy)
-#ddy <- density(modey1)
-#which.max(ddy$y)
-#modey <- ddy$x[which.max(ddy$y)]
+# dNBR(corr)
+if(FALSE){ # Do not need corrected dNBR
+  # Mode calculation i
+  modei1 <- (preNBRi - postNBRi)
+  ddi <- density(modei1)
+  which.max(ddi$y)
+  modei <- ddi$x[which.max(ddi$y)]
+  
+  # Mode calculation y
+  modey1 <- (preNBRy - postNBRy)
+  ddy <- density(modey1)
+  which.max(ddy$y)
+  modey <- ddy$x[which.max(ddy$y)]
 
-##raster calculations
+  # Raster calculations
+  dNBRicorr <- ((preNBRi - postNBRi) - modei)
+  dNBRycorr <- ((preNBRy - postNBRy) - modey)
+}
 
-#dNBRicorr <- ((preNBRi - postNBRi) - modei)
-#dNBRycorr <- ((preNBRy - postNBRy) - modey)
-
-#dNBR
+# dNBR
 dNBRi <- preNBRi - postNBRi
 dNBRy <- preNBRy - postNBRy
 
-#RbNBR
+# RbNBR
 RdNBRi <- (dNBRi / (sqrt(abs(preNBRi / 1000))))
 RdNBRy <- (dNBRy / (sqrt(abs(preNBRi / 1000))))
 
-#NDMI
+# NDMI
 NDMIi <- (postB5i - postB6i) / (postB5i + postB6i)
 NDMIy <- (postB5y - postB6y) / (postB5y + postB6y)
 
-#B5B4 (B6/B5 in Landsat 8)
+# B5B4 (B6/B5 in Landsat 8)
 B5B4i <- postB6i / postB5i
 B5B4y <- postB6y / postB5yvaluetable <- na.omit(getValues(trainingbrick))
 
-#NDVI
+# NDVI
 NDVIi <- (postB5i - postB4i) / (postB5i + postB4i)
 NDVIy <- (postB5y - postB4y) / (postB5y + postB4y)
 
-#TCBRI
+# TCBRI
 TCBRIi <- 0.2049 * postB2i + 0.4158 * postB3i + 0.5524 * postB4i + 0.5741 * postB5i + 0.3124 * postB6i + 0.2303 * postB7i
 TCBRIy <- 0.2049 * postB2y + 0.4158 * postB3y + 0.5524 * postB4y + 0.5741 * postB5y + 0.3124 * postB6y + 0.2303 * postB7y
 
-#TCGRE
+# TCGRE
 TCGREi <- -0.1603 * postB2i - 0.2819 * postB3i - 0.4934 * postB4i + 0.79401 * postB5i - 0.0002 * postB6i - 0.1446 * postB7i
 TCGREy <- -0.1603 * postB2y - 0.2819 * postB3y - 0.4934 * postB4y + 0.79401 * postB5y - 0.0002 * postB6y - 0.1446 * postB7y
 
-#TCWET
+# TCWET
 TCWETi <-0.0315 * postB2i + 0.2021 * postB3i + 0.3102 * postB4i + 0.1594 * postB5i - 0.6806 * postB6i - 0.6109 * postB7i
 TCWETy <-0.0315 * postB2y + 0.2021 * postB3y + 0.3102 * postB4y + 0.1594* postB5y - 0.6806 * postB6y - 0.6109 * postB7y
 
-#write rasters
+# Write rasters
 rasternames <- c("RdNBRi", "RdNBRy", "dNBRi", "dNBRy", "NDMIi", "NDMIy", "B5B4i", "B5B4y", "NDVIi", "NDVIy", "TCBRIi", "TCBRIy", "TCGREi", "TCGREy", "TCWETi", "TCWETy")
 rasterlist <- mget(rasternames)
 covs <- stack(rasterlist)
 
-#remove intermediate files
+# Remove intermediate files
 remove <- c(files, rasternames, "postNBRi", "postNBRy", "preNBRi", "preNBRy")
 rm(list = remove)
 gc()
 
-#Create Random points within fire boundary (specify number of points)
+# Create Random points within fire boundary (specify number of points)
 library(splancs)
 rndmpts <- spsample(KingOutline, n = 300, type = "random")
 projection(rndmpts)  <- projection(KingOutline)
@@ -131,47 +132,47 @@ rndmptsdf <- data.frame(FID = row.names(rndmpts), X100pc = rep(NA, times = lengt
 rndmptsdf <- SpatialPointsDataFrame(rndmpts, data = df, proj4string = project(KingOutline))
 writeOGR(obj = rndmptsdf, dsn = getwd(), layer = "rndmpts", driver = "ESRI Shapefile", overwrite_layer = T)
 
-#Stop to Identify with "Ground truthing" methods
+# Stop script to identify with "Ground truthing" methods
 stop("Visually identify areas of 100% mortality at point locations at this time")
 
-#import points
-#Spoints1 = readOGR('rndmpts.shp')
-#Spoints2 = readOGR('/media/antho/HDD/KingFire/Capstone/R/Capstone/Burned100pc.shp')
-#Spoints <- readOGR('Newrndmpts.shp')
+# Import random sample points
+Spoints1 = readOGR('rndmpts.shp')
+Spoints2 = readOGR('/media/antho/HDD/KingFire/Capstone/R/Capstone/Burned100pc.shp')
+Spoints <- readOGR('Newrndmpts.shp')
 proj4string(Spoints) <- proj4string(covs)
 plot(Spoints1)
 plot(Spoints2, add = T)
 
-
-#rasterize points
+# Rasterize points
 SRaster <- rasterize(Spoints, covs, field = 'X100pc')
 
-#mask data to points
+# Mask data to points
 covmask <- mask(covs, SRaster)
 
-#combine covariates with sample data to make training dataset
+# Combine covariates with sample data to make training dataset
 names(SRaster) <- "Burned100"
 trainingbrick <- addLayer(covmask, SRaster)
 
-#Extract values to matrix
+# Extract values to matrix
 valuetable <- na.omit(getValues(trainingbrick))
 valuetable <- data.frame(valuetable)
 valuetable$Burned100 <- as.factor(valuetable$Burned100)
 
-#random forest model
+# Random forest model
 rfm1 <- randomForest(Burned100 ~., data = valuetable, importance = T)
 rfm1.error <- mean(rfm1$err.rate)
 rfm1i <- rfm1
 rfm1i$importance[,"MeanDecreaseAccuracy"] <- rfm1i$importance[,"MeanDecreaseAccuracy"]*100
 varImpPlot(rfm1i, main = "Burn Severity Classification Model", type = 1, scale=F)
 plot(rfm2)
-#use rfm1 to predict
+
+# Use rfm1 to predict
 predBurn <- predict(covs, model = rfm1, filename = "predBurn.asc", format = "ascii", na.rm = TRUE, inf.rm = TRUE, overwrite = TRUE)
 proj4string(predBurn) <- proj4string(covs)
 predBurn <- round(predBurn, digits = 0)
 plot(predBurn)
 
-#plot proportion of classified points
+# Plot proportion of classified points
 h <- hist(predBurn, breaks=2)
 c.t <- sum(h$counts) #total cells
 c0 <- h$counts[1] #count of 0s
@@ -187,8 +188,8 @@ text(.75, c1 / 2, labels = paste(round(p1*100, digits = 1), "%"), col = "white",
 
 ######  Prediction of Burn Severity Using LiDAR  ######
 
-##Setup
-#load libraries
+###   Initial setup   ###
+# Load libraries
 library(sp)
 library(raster)
 library(rgdal)
@@ -198,23 +199,23 @@ library(doParallel)
 library(randomForest)
 library(GISTools)
 
-#set working directory
+# Set working directory
 setwd("D:/KingFire/Capstone/R/Capstone")
 
 ##Input spatial files
-#fire perimeter
+# Fire perimeter
 KingOutline <- readOGR('D:/KingFire/Capstone/R/Capstone/KingOutline.shp')
 
-#bare earth raster
+# Bare earth raster
 ground <- raster("D:/KingFire/CoverStrata/BareEarthKing.asc")
 
-#100% burned raster (output from random forest #1)
+# 100% burned raster (output from random forest #1)
 predBurn <- raster('predBurn.asc')
 
-#LAS delivery tiles
+# LAS delivery tiles
 DelivTiles <- readOGR('D:/KingFire/Capstone/R/Capstone/LAS_DeliveryTiles.shp')
 
-##List intersecting LAS files
+## List intersecting LAS files
 KingOutlineProj <- spTransform(KingOutline, projection(DelivTiles))
 plot(KingOutlineProj)
 plot(DelivTiles, add = TRUE)
@@ -225,8 +226,8 @@ InputFiles <- paste0("D:/KingFire/PreBurn/Raw/Points/FullCloud/", Ipf)
 InputFiles <- sort(InputFiles)
 InputFileNames <- gsub(".las", "", gsub("D:/KingFire/PreBurn/Raw/Points/FullCloud/", "", InputFiles, fixed = TRUE))
 
-##Import LAS files, normalize them, perform grid statistics, and remove (large) intermediate files
-#abridge list
+## Import LAS files, normalize them, perform grid statistics, and remove (large) intermediate files
+# Abridge list
 InputFiles <- InputFiles[1:2]
 InputFileNames <- InputFileNames[1:2]
 
@@ -253,7 +254,7 @@ stopCluster(cl)
 for(i in 1:length(x)) {
   LAS_Grid <- rbind(LAS_Grid, x[[i]])}
 
-##Produce rasters
+# Produce rasters
 StdMetrics <- colnames(LAS_Grid[,-c(1:2)])
 LASrNames <-paste0("LAS_Raster_", StdMetrics)
 LAS_Grid_sp <- LAS_Grid
@@ -278,26 +279,25 @@ covmask2 <- mask(covs2, KingOutline)
 predmask <- crop(predmask, covmask2)
 trainingbrick2 <- addLayer(covmask2, predmask)
 
-#Extract values to matrix
+# Extract values to matrix
 valuetable2 <- na.omit(getValues(trainingbrick2))
 valuetable2 <- data.frame(valuetable2)
 valuetable2$predBurn <- as.factor(round(valuetable2$predBurn))
 
-#random forest model
+# Random forest model
 rfm2 <- randomForest(predBurn ~., data = valuetable2, importance = T)
 varImpPlot(rfm2)
 rfm2.error <- mean(rfm2$err.rate)
 rfm2$err.rate
 str(rfm2)
 
-#use rfm2 to predict
+# Use rfm2 to predict
 predStrctr <- predict(covmask2, model = rfm2, filename = "predStrctr.asc", format = "ascii", na.rm = TRUE, overwrite = TRUE)
 predStrctr.prob <- predict(covmask2, model = rfm2, type = "prob")
 plot(predStrctr.prob)
 plot(predStrctr)
 plot(predmask)
 
-#
 predBurn.str.mask <- mask(crop(predBurn, predStrctr), predStrctr)
 plot(predBurn.str.mask)
 plot(predStrctr.prob, add = T)
@@ -311,7 +311,7 @@ cellStats(predStrctr.error, stat = 'sum', na.rm = T)
 cellStats(predStrctr.error2, stat = 'sum', na.rm = T)
 rfm2$confusion
 
-#rfm3 most important
+# Create Random Forest from most important variables (rfm3)
 imp.list <- c("predBurn","zpcum1","zpcum2","zpcum3","zpcum4","zpcum5","zpcum6","zpcum7","zpcum8","zpcum9","zkurt","zskew")
 valuetable3 <- valuetable2[imp.list]
 rfm3 <- randomForest(predBurn ~., data= valuetable3, importance = T)
@@ -336,8 +336,7 @@ colors <- c("#5C5C76", "#F9EB38", "#F5BF1A", "#CD6D3A", "#9F4837")
 plot(f2, breaks = breakpoints, col = colors, axes = FALSE, box = OFF, legend = FALSE)
 writeRaster(f2, "KingStandReplacement.asc", "ascii", overwrite = TRUE)
 
-
-#distance PDF
+# Export Distance as PDF
 brk <- c("Surviving Forest Canopy", "< 200 m", "200 - 400 m", "400 - 800 m", "> 800 m")
 par(mar=c(1,1,1,1))
 pdf("KingStandReplacement.pdf", width = 7, height = 10, paper = "letter")
@@ -348,7 +347,7 @@ north.arrow(xmin(f2) + 7000, ymin(f2) + 37000, 500, lab = "N")
 map.scale(xmin(f2) + 7000, ymin(f2) + 35000, 10 * 1000, "Km", 5, 2)
 dev.off()
 
-#distance PDF
+# Export Distance as PDF
 f3 <- round(predBurn)
 f3 <- mask(f3, KingOutline)
 brk2 <- c("100% Tree Mortality", "< 100% Tree Mortality")
@@ -361,7 +360,7 @@ north.arrow(xmin(f2) + 7000, ymin(f2) + 37000, 500, lab = "N")
 map.scale(xmin(f2) + 7000, ymin(f2) + 35000, 10 * 1000, "Km", 5, 2)
 dev.off()
 
-#distance jpeg
+# Export Distance as jpeg
 par(mar=c(1,1,1,1))
 jpeg("KingStandReplacement.jpeg", width = 7, height = 10, units = "in", res = 300, family = "", type = "windows")
 plot(f2, breaks = breakpoints, col = colors, axes = FALSE, box = FALSE, legend = FALSE, main = "King Fire - Stand replacement area", add = FALSE)
@@ -371,7 +370,7 @@ north.arrow(xmin(f2) + 7000, ymin(f2) + 37000, 500, lab = "N")
 map.scale(xmin(f2) + 7000, ymin(f2) + 35000, 10 * 1000, "Km", 5, 2)
 dev.off()
 
-#burn prediction jpeg
+# Export Burn prediction as jpeg
 par(mar=c(1,1,1,1))
 jpeg("BurnPrediction.jpeg", width = 7, height = 10, units = "in", res = 300, family = "", type = "windows")
 plot(f3, breaks = c(0,.9,1.5), col = c("#5C5C76", "#CD6D3A"), axes = FALSE, box = FALSE, legend = FALSE, main = "King Fire - Burn Mortality Status", add = FALSE)
@@ -381,7 +380,7 @@ north.arrow(xmin(f2) + 7000, ymin(f2) + 37000, 500, lab = "N")
 map.scale(xmin(f2) + 7000, ymin(f2) + 35000, 10 * 1000, "Km", 5, 2)
 dev.off()
 
-#LiDAR Prediction jpeg
+# Export LiDAR Prediction as jpeg
 par(mar=c(1,1,1,1))
 jpeg("LiDARPrediction.jpeg", width = 7, height = 10, units = "in", res = 300, family = "", type = "windows")
 plot(predBurn.str.mask, breaks = c(0,.9,1.5), col = c("#5C5C76", "#CD6D3A"), axes = FALSE, box = FALSE, legend = FALSE, main = "King Fire - Prediction of Burn severity using LiDAR", add = FALSE)
